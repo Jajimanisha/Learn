e005fc0886d5ec6e35e3e97d8bb286e2a6135d82
pichi <pichi@LAPTOP-ANUP6RKU>
2020-03-28 17:09:08 +0530

    Added dependencies and xml file

--- project/src/test/java/com/company/project/Testing.java
+++ project/src/test/java/com/company/project/Testing.java
@@ -1,10 +1,14 @@
 package com.company.project;
 
+import org.testng.annotations.Test;
+
 public class Testing {
 
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	@Test
+	public void method1() {
 System.out.println("hi");
+	
 	}
-
 }
